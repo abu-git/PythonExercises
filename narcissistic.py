7c87e8445f42d8da063997246ab81b1db41dd75d
abu-git <abu.sammie@gmail.com>
2020-02-03 21:06:48 +0200

    python practice 03 Feb 2020 09:07PM

--- narcissistic.py
+++ narcissistic.py
@@ -1,5 +1,6 @@
 """
-A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of 
+the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
 
 For example, take 153 (3 digits):
 
@@ -11,13 +12,15 @@ and 1634 (4 digits):
 
 The Challenge:
 
-Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+Your code must return true or false depending upon whether the given number is a Narcissistic number 
+in base 10.
 
-Error checking for text strings or other invalid inputs is not required, only valid integers will be passed into the function.
+Error checking for text strings or other invalid inputs is not required, only valid integers will 
+be passed into the function.
 """
 
 def narcissistic(value):
-	the_power = len(str(value))
+	the_power = len(str(value)) 
 	digits = [int(i) for i in str(value)] ####------Split Integer to Digits
 	i = 0
 	result = 0
---
+++ next_bigger.py
@@ -0,0 +1,48 @@
+"""
+You have to create a function that takes a positive integer number and returns the next bigger 
+number formed by the same digits:
+
+	12 ==> 21
+	513 ==> 531
+	2017 ==> 2071
+
+If no bigger number can be composed using those digits, return -1:
+
+	9 ==> -1
+	111 ==> -1
+	531 ==> -1
+"""
+
+def next_bigger(n):
+	stringify = [int(i) for i in str(n)]
+	result = 0
+	string_result = ""
+	if(len(stringify) == 1):
+		return -1
+	if(len(stringify) > 1):
+		i = -1
+		
+		while(result < n):
+			last_digit = stringify[i]
+			before_last = stringify[i - 1]
+			stringify[i] = before_last
+			stringify[i - 1] = last_digit
+
+			for c in stringify:
+				string_result = string_result + str(c)
+			result = int(string_result)
+			i = i - 1
+			if(result > n):
+				break					
+	if(result > n):
+		return result
+	else:
+		return -1
+	
+
+
+print(next_bigger(12))
+print(next_bigger(513))
+print(next_bigger(2017))
+print(next_bigger(414))
+print(next_bigger(144))
